b0a268a8bbe20c7769b206f888cbeecb00be1464
Rohit Kumar <r.k2962002@gmail.com>
2023-02-13 00:09:36 +0530

    #

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
 import Home from "./components/home/Home";
+import axios from "axios";
 
 function App() {
+  React.useEffect(() => {
+    axios
+      .get("https://resumedps.aboutrohit.in")
+      .then((res) => console.log(res.data));
+  }, []);
+
+
   return (
     <>
       <Home />
